23af4c40bd2a478f03d9619616028df3939da123
Alisher <a.aituarov@ibreal.dev>
2021-12-21 22:31:43 +0600

    wip

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,9 +6,23 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist")
   },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html")
+      template: path.join(__dirname, "public", "index.html")
     })
   ]
 }
